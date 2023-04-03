1eb2ea177dc75b145b966b0c7d3f853454283120
VladVrt <117577628+VladVrt@users.noreply.github.com>
2023-04-03 20:27:33 +0300

    Done Fundamentals Part 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let repeatString = string;
+  if (num == 0) {
+    repeatString = "";
+  } else if (num < 0) {
+    repeatString = "ERROR";
+  } else {
+    for (let i = 1; i < num; i++) {
+      repeatString += string;
+    }
+  }
+  return repeatString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  i = num1;
+  y = num2;
+  if (i < 0 || y < 0 || Number(i)!==i || Number(y)!==y) {
+    return "ERROR";
+  } else if (i < y) {
+    for (i; i <= y; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (i > y) {
+    for (y; y <= i; y++) {
+      sum += y;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
